b78ccca994ed29f0c5eca43b37b20050abd8d042
YutaHosoya <155717183+zero7500@users.noreply.github.com>
2024-02-20 04:39:37 +0000

    [Finish]アプリケーションを完成させよう課題１

---
+++ bookers/app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      flash.now[:alert]
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+     @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      flash.now[:alert]
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ bookers/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  root 'homes#top'
+  resources :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
